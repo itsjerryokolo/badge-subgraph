ed70d58c61876959a78436ea9bf73a3bff0efbfc
Jerry Okolo <jerrycreative@live.co.uk>
2023-12-01 11:07:00 +0100

    initial commit

--- src/badge.ts
+++ src/badge.ts
@@ -1,46 +1,30 @@
+import { log } from "matchstick-as";
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/Badge/Badge"
-import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
+	ProtocolSettingsUpdated,
+	Badge,
+	BadgeRequested,
+} from "../generated/Badge/Badge";
+import { ProtocolConfig } from "../generated/schema";
 
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+export function handleProtocolSettingsUpdated(
+	event: ProtocolSettingsUpdated
+): void {
+	const theBadgeAddress = event.address;
+	const theBadge = Badge.bind(theBadgeAddress);
+	const tbSTore = theBadge.try__badgeStore();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	if (!tbSTore.reverted) {
+		let protocolConfigs = ProtocolConfig.load(tbSTore.value);
 
-  entity.save()
-}
+		if (!protocolConfigs) {
+			protocolConfigs = new ProtocolConfig(tbSTore.value);
+		}
 
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+		protocolConfigs.address = tbSTore.value;
+		protocolConfigs.save();
+	} else {
+		log.warning("try__badgeStore reverted: {}", [
+			event.transaction.hash.toHexString(),
+		]);
+	}
 }
